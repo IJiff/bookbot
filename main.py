9ae003182c151ab5b4d61a667877d002d7431574
Dallin <ijiff08@gmail.com>
2025-03-03 11:28:21 -0700

    add sorted_list and info message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import count_words
 from stats import count_chars
+from stats import create_sorted_list
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -10,7 +11,15 @@ def main():
     book = get_book_text("books/frankenstein.txt")
     count = count_words(book)
     print(f"{count} words found in the document")
-    print(count_chars(book))
-
+    char_counts = count_chars(book)
+    count_list = create_sorted_list(char_counts)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {count} total words")
+    print("--------- Character Count -------")
+    for entry in count_list:
+        print(f"{entry["char"]}: {entry["count"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -14,3 +14,16 @@ def count_chars(book):
             char_counts[c] = 0
         char_counts[c] += 1
     return char_counts
+
+
+def create_sorted_list(counts):
+    letter_list = []
+    for c in counts:
+        if c.isalpha():
+            letter_list.append({"char": c, "count": counts[c]})
+    letter_list.sort(reverse=True, key=sort_on)
+    return letter_list
+
+
+def sort_on(dict):
+    return dict["count"]
